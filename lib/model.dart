486eb7b0e40ce7c46e6abc3952691acef9040341
Eran Hsu <eran0926@gmail.com>
2025-01-28 22:37:13 +0800

    feat(model):add model for auto data

---
+++ lib/model.dart
@@ -0,0 +1,32 @@
+enum Preload {
+  none,
+  coral,
+  algae,
+}
+
+enum AutoStartPosition {
+  side,
+  center,
+  middle,
+}
+
+enum AutoPathPoint{
+//   TODO: Add more points
+  left,
+  right,
+  center,
+}
+
+enum TelopPathPoint{
+  // TODO: Add more points
+  left,
+  right,
+  center,
+}
+
+enum BargeAction {
+  none,
+  park,
+  deep,
+  shallow,
+}
