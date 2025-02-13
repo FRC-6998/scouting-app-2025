0aae8eece6eb1afeb0d527e1534fea7fa2ab153b
Eran Hsu <eran0926@gmail.com>
2025-02-13 20:05:15 +0800

    feat(schema):add qrcode data_type

--- lib/providers/scouting_data_provider.dart
+++ lib/providers/scouting_data_provider.dart
@@ -218,6 +218,7 @@ class ScoutingData extends ChangeNotifier {
       _ulid = Ulid().toString();
     }
     return {
+      'data_type': 'objective_data',
       'ulid': _ulid,
       'scout': scout,
       'match_level': matchLevel.name,
